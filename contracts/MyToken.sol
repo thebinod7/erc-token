58c637e6358d55c6597a751b3ba85fb08eb63380
Binod Chaudhary <binod@rumsan.com>
2021-06-21 08:48:50 +0545

    mint burn and transfer token

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,5 +4,19 @@ pragma solidity ^0.8.5;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-	 constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+	address public adminAddress;
+
+	 constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+		 _mint(msg.sender, 20 * 10 ** 18); 
+		 adminAddress = msg.sender;
+	 }
+
+	 function mint(address to, uint amount) external {
+		 require(msg.sender == adminAddress, 'Only admin can mint');
+		 _mint(to, amount);
+	 }
+
+	 function burn(uint amount) external {
+		 _burn(msg.sender, amount);
+	 }
 }
